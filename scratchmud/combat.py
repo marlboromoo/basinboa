c0202311c8a7b5e51923b7f358c77c5181437568
timothy.lee <marlboromoo@gmail.com>
2013-01-24 18:01:56 +0800

    notice all players in the same room when fight.

--- scratchmud/combat.py
+++ scratchmud/combat.py
@@ -4,6 +4,7 @@ combat system.
 """
 
 from scratchmud import status
+from scratchmud.message import mob_message_to_room, character_message_to_room
 
 def fight():
     """docstring for fight"""
@@ -14,9 +15,13 @@ def fight():
             character.client.send("you hit %s cause damage %s!\n" % (target.get_name(), '10'))
             if target.is_player():
                 target.client.send("%s hit you cause damage %s!\n" % (character.get_name(), '10'))
+            character_message_to_room(character, "%s hit %s cause damage %s!\n" % 
+                                      (character.get_name(), target.get_name(), '10'))
             #. target hit player
             target.hurt(character)
             character.client.send("%s hit you cause damage %s!\n" % (target.get_name(), '10'))
+            character_message_to_room(character, "%s hit %s cause damage %s!\n" % 
+                                      (target.get_name(), character.get_name(), '10'))
             #. send prompt
             character.send_prompt()
             
