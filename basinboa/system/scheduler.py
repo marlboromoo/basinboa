cd403b450910a0b170f20a153a4d1c3f52e058f2
timothy.lee <marlboromoo@gmail.com>
2013-01-31 09:15:23 +0800

    Cycle should not check the time.

--- basinboa/system/scheduler.py
+++ basinboa/system/scheduler.py
@@ -4,7 +4,7 @@ scheduler in the world.
 reference: http://code.google.com/p/bogboa/source/browse/trunk/mudlib/sys/scheduler.py
 """
 import time
-#import random
+import random
 
 class Cycle(object):
     """ add a event to scheduler periodical """
@@ -17,25 +17,30 @@ class Cycle(object):
         self.time = None
         self._add()
 
+    def __repr__(self):
+        return "Cycle: %s, args:%s, delay:%s, fire_time:%s" % (
+            self.func, self.args, self.delay, self.time)
+
     def _add(self):
         """docstring for _add"""
         SCHEDULER.add(self.delay, self.fire, self.args)
 
-    def _can_add(self):
-        """docstring for _can_add"""
-        if not self.previous_time:
-            self.previous_time = time.time()
-        self.time = time.time()
-        if self.time - self.previous_time >= self.delay:
-            self.time, self.previous_time = None, None
-            return True
-        return False
+    #def _can_add(self):
+    #    """docstring for _can_add"""
+    #    if not self.previous_time:
+    #        self.previous_time = time.time()
+    #    self.time = time.time()
+    #    if self.time - self.previous_time >= self.delay:
+    #        self.time, self.previous_time = None, None
+    #        return True
+    #    return False
 
     def fire(self, args=()):
         """docstring for fire"""
         self.func(self.args) if len(self.args) > 0 else self.func()
-        if self._can_add:
-            self._add()
+        #if self._can_add:
+        #    self._add()
+        self._add()
         
 class Event(object):
     """function wrapper"""
@@ -85,25 +90,27 @@ class Scheduler(object):
 
 SCHEDULER = Scheduler()
 
-#if __name__ == '__main__':
-#    def echo(msg):
-#        """docstring for echo"""
-#        print msg
-#
-#    def ping():
-#        """docstring for ping"""
-#        print 'pong!'
-#
-#    Cycle(2, echo, 'cycle!')
-#    Cycle(2, ping)
-#    SCHEDULER.add(1, ping)
-#    SCHEDULER.add(1, echo, 'echo!')
-#    #SCHEDULER.add(3, echo, 'foo')
-#    while True:
-#        time.sleep(1)
-#        print "%s %s" % (SCHEDULER.time, SCHEDULER.get_events())
-#        SCHEDULER.tick()
-#        if random.choice([True, False]):
-#            SCHEDULER.add(random.choice([1,2,3]), echo, random.choice(['foo', 'bar', 'spam']))
+#------------------------------------------------------------------------------
+#       Debug
+#------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    def echo(msg):
+        print msg
+
+    def ping():
+        print 'pong!'
+
+    Cycle(2, echo, 'cycle!')
+    Cycle(2, ping)
+    SCHEDULER.add(1, ping)
+    SCHEDULER.add(1, echo, 'echo!')
+    #SCHEDULER.add(3, echo, 'foo')
+    while True:
+        time.sleep(1)
+        print "%s %s" % (SCHEDULER.time, SCHEDULER.get_events())
+        SCHEDULER.tick()
+        if random.choice([True, False]):
+            SCHEDULER.add(random.choice([1,2,3]), echo, random.choice(['foo', 'bar', 'spam']))
 
 
