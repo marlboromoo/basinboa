854e26c654459a506adb482d9d6b812ffc356867
timothy.lee <marlboromoo@gmail.com>
2013-02-04 09:39:08 +0800

    fix bug: item will be override when item has the same name.

--- basinboa/mobile/bag.py
+++ basinboa/mobile/bag.py
@@ -9,25 +9,32 @@ class Bag(object):
         super(Bag, self).__init__()
         self.max_burden = 100
         self.burden = 0
-        self.items = {} #.key is item name, value is Item object
+        self.items = {} #.key is item name, value is list that contain Item objects
         
     def checkin(self, item):
         """docstring for checkin"""
         if self.burden < self.max_burden:
-            self.items[item.get_name()] = item
+            if self.items.has_key(item.get_name()):
+                self.items[item.get_name()].append(item)
+            else:
+                self.items[item.get_name()] = [item]
             return True
         return False
 
     def checkout(self, item_name):
         """docstring for checkout"""
         if self.items.has_key(item_name):
-            return self.items.pop(item_name)
+            if len(self.items[item_name]) == 1:
+                item = self.items.pop(item_name)[0]
+            else:
+                item = self.items[item_name].pop(0)
+            return item
         else:
             return None
 
     def checkout_all(self):
         """docstring for list_items"""
-        items = self.items.values()
+        items = self.list_items()
         self.remove_items()
         return items
 
@@ -37,9 +44,12 @@ class Bag(object):
 
     def list_items(self):
         """docstring for list"""
-        return self.items.values()
+        items = []
+        for list_ in self.items.values():
+            items.extend(list_)
+        return items
 
     def count_items(self):
         """docstring for count"""
-        return len(self.items)
+        return len(self.list_items())
 
